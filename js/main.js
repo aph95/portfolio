e963d7a9c93fdb22bf27c770f782e76b0bc2d065
aph95 <150137879+aph95@users.noreply.github.com>
2024-11-12 23:18:54 +0100

    indent

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var textWrapper = document.querySelector('.ml2');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: false})
+anime.timeline({ loop: false })
   .add({
     targets: '.ml2 .letter',
-    scale: [4,1],
-    opacity: [0,1],
+    scale: [4, 1],
+    opacity: [0, 1],
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 950,
-    delay: (el, i) => 70*i
+    delay: (el, i) => 70 * i
   }).add({
     targets: '.ml2',
     opacity: 1,
@@ -17,3 +17,13 @@ anime.timeline({loop: false})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const toggler = document.querySelector('.navbar-toggler');
+
+  // Event spy
+  toggler.addEventListener('click', function () {
+    // Toggle the collapsed class immediately on click
+    toggler.classList.toggle('collapsed');
+  });
+});
