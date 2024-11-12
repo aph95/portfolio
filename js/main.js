babe2564d222f0bf0ffefc4ed0c19cf7a46bff42
aph95 <150137879+aph95@users.noreply.github.com>
2024-11-12 22:56:55 +0100

    aa

--- js/main.js
+++ js/main.js
@@ -17,23 +17,3 @@ anime.timeline({loop: false})
     easing: "easeOutExpo",
     delay: 1000
   });
-
-  document.querySelectorAll('.nav-link.ml2').forEach(item => {
-    item.innerHTML = item.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-});
-
-// Anime.js animation
-anime.timeline({ loop: false })
-  .add({
-    targets: '.nav-link .letter', // Target each letter in each nav-link
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: (el, i) => 70 * i  // Delay each letter individually
-  });
-
-  // new
-
-  
