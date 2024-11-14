8bece3089aa8f0ce777e9fcac1d89546bf7ca66a
aph95 <150137879+aph95@users.noreply.github.com>
2024-11-14 19:56:54 +0100

    ll

--- js/main.js
+++ js/main.js
@@ -1,37 +1,28 @@
-// Event listener for the navbar toggler
-document.addEventListener('DOMContentLoaded', function () {
-  const toggler = document.querySelector('.navbar-toggler');
-  toggler.addEventListener('click', function () {
-    toggler.classList.toggle('collapsed');
-  });
-});
-
-anime({
-  targets: '.arrow-image',  // Target the iPhone image
-  translateY: [-20, 20],     // Float up and down by 20px
-  direction: 'alternate',    // Reverse the direction on each cycle
-  easing: 'easeInOutSine',   // Smooth easing function
-  duration: 1000,            // 2 seconds per cycle
-  loop: true                 // Loop the animation infinitely
-});
-
-// Text animation for "Let's bring your ideas to life."
+// Wrap every letter in a span
 var textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+// Add the lightbulb as a 'letter' element
+const lightbulb = document.createElement('span');
+lightbulb.innerHTML = '<i class="fa-solid fa-lightbulb"></i>';
+textWrapper.appendChild(lightbulb);
+lightbulb.classList.add('letter', 'lightbulb');
+
+// Text and lightbulb animation
 anime.timeline({loop: false})
   .add({
     targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
+    translateY: ["1.1em", 0],  // Make it jump up and down like the text
     translateZ: 0,
+    opacity: [0, 1],  // Fade the letters in
     duration: 750,
-    delay: (el, i) => 50 * i
+    delay: (el, i) => 50 * i  // Staggered delay for each letter
   })
   .add({
-    targets: '.fa-lightbulb', // Target the lightbulb icon
-    opacity: [0, 1],           // Fade in the lightbulb
-    scale: [0, 1],             // Scale up the lightbulb
-    duration: 500,             // Duration of the lightbulb animation
-    easing: 'easeInOutSine',   // Easing function for smooth transition
-    offset: '-=300'            // Start after the text animation ends, adjust as needed
+    targets: '.ml6 i.fa-lightbulb',  // Target the lightbulb icon
+    opacity: [0, 1],  // Fade in the lightbulb
+    translateY: ["1.1em", 0],  // Same jump as text
+    translateZ: 0,
+    duration: 750,
+    delay: anime.stagger(50, {start: 50})  // Lightbulb follows the staggered delay
   });
