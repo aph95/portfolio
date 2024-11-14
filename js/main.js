63582dbef73bf0c2cf1554272864e4821eda5e3d
aph95 <150137879+aph95@users.noreply.github.com>
2024-11-14 19:58:07 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// Event listener for the navbar toggler
+document.addEventListener('DOMContentLoaded', function () {
+  const toggler = document.querySelector('.navbar-toggler');
+  toggler.addEventListener('click', function () {
+    toggler.classList.toggle('collapsed');
+  });
+});
+
+anime({
+  targets: '.arrow-image',  // Target the iPhone image
+  translateY: [-20, 20],     // Float up and down by 20px
+  direction: 'alternate',    // Reverse the direction on each cycle
+  easing: 'easeInOutSine',   // Smooth easing function
+  duration: 1000,            // 2 seconds per cycle
+  loop: true                 // Loop the animation infinitely
+});
+
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
