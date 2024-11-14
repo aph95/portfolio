56df3d1cfd906b67ecfe33e51c202313188a1359
aph95 <150137879+aph95@users.noreply.github.com>
2024-11-14 21:14:29 +0100

    v

--- js/main.js
+++ js/main.js
@@ -51,14 +51,12 @@ anime.timeline({loop: false})
     const navbar = document.querySelector('.navbar');
   
     function toggleNavbar() {
-      // Only apply hide/show on screens wider than 768px
       if (window.innerWidth > 768) {
         window.addEventListener('scroll', function () {
           let currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   
-          // Show or hide the navbar based on the scroll direction
           if (currentScrollPosition > lastScrollPosition && currentScrollPosition > 50) {
-            navbar.style.top = '-100px'; // Moves it out of view
+            navbar.style.top = '-100px'; // Moves it out of view smoothly
           } else {
             navbar.style.top = '0';
           }
@@ -66,15 +64,11 @@ anime.timeline({loop: false})
           lastScrollPosition = currentScrollPosition;
         });
       } else {
-        // For smaller screens, keep the navbar always visible
         navbar.style.top = '0';
       }
     }
   
-    // Initial check
     toggleNavbar();
   
-    // Recheck on window resize to handle orientation changes
     window.addEventListener('resize', toggleNavbar);
   });
-  
